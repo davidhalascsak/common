1c7afb03633a90330c292a27b43029161e5363b3
David Halascsak <dhalascsak@altair.com>
2024-09-02 15:28:00 +0200

    Update date format

--- src/logging.cc
+++ src/logging.cc
@@ -80,12 +80,11 @@ LogMessage::LogTimestamp(std::stringstream& stream)
       break;
     }
     case Logger::Format::kALTAIR: {
-      stream << std::setfill('0') << std::setw(2) << timestamp_.wMonth
-             << std::setw(2) << timestamp_.wDay << ' ' << std::setw(2)
-             << timestamp_.wHour << ':' << std::setw(2) << timestamp_.wMinute
-             << ':' << std::setw(2) << timestamp_.wSecond << '.' << std::setw(6)
-             << timestamp_.wMilliseconds * 1000;
-      break;
+      stream << timestamp_.wYear << '-' << std::setfill('0') << std::setw(2)
+             << timestamp_.wMonth << '-' << std::setw(2) << timestamp_.wDay
+             << 'T' << std::setw(2) << timestamp_.wHour << ':' << std::setw(2)
+             << timestamp_.wMinute << ':' << std::setw(2) << timestamp_.wSecond
+             << "Z";
     }
     case Logger::Format::kISO8601: {
       stream << timestamp_.wYear << '-' << std::setfill('0') << std::setw(2)
@@ -114,11 +113,11 @@ LogMessage::LogTimestamp(std::stringstream& stream)
       break;
     }
     case Logger::Format::kALTAIR: {
-      stream << std::setfill('0') << std::setw(2) << (tm_time.tm_mon + 1)
-             << std::setw(2) << tm_time.tm_mday << ' ' << std::setw(2)
-             << tm_time.tm_hour << ':' << std::setw(2) << tm_time.tm_min << ':'
-             << std::setw(2) << tm_time.tm_sec << '.' << std::setw(6)
-             << timestamp_.tv_usec;
+      stream << (tm_time.tm_year + 1900) << '-' << std::setfill('0')
+             << std::setw(2) << (tm_time.tm_mon + 1) << '-' << std::setw(2)
+             << tm_time.tm_mday << 'T' << std::setw(2) << tm_time.tm_hour << ':'
+             << std::setw(2) << tm_time.tm_min << ':' << std::setw(2)
+             << tm_time.tm_sec << "Z";
       break;
     }
     case Logger::Format::kISO8601: {
