73378691cbe1cc4a7aaf387120a817c1a9f1dcbf
David Halascsak <dhalascsak@altair.com>
2024-09-02 15:10:51 +0200

    Update log level names

--- include/triton/common/logging.h
+++ include/triton/common/logging.h
@@ -62,7 +62,7 @@ class Logger {
   enum class Level : uint8_t { kERROR = 0, kWARNING = 1, kINFO = 2, kEND };
 
   inline static const std::array<const char*, static_cast<uint8_t>(Level::kEND)>
-      LEVEL_NAMES{"E", "W", "I"};
+      LEVEL_NAMES{"ERROR", "WARNING", "INFO"};
 
   Logger();
 
