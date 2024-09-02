1f91c19578e2fd30b463be7b45efdc3fb8c11667
David Halascsak <dhalascsak@altair.com>
2024-09-02 17:20:35 +0200

    Update json format

--- src/logging.cc
+++ src/logging.cc
@@ -114,11 +114,11 @@ LogMessage::LogTimestamp(std::stringstream& stream)
       break;
     }
     case Logger::Format::kALTAIR: {
-      stream << (tm_time.tm_year + 1900) << '-' << std::setfill('0')
+      stream << "\"" << (tm_time.tm_year + 1900) << '-' << std::setfill('0')
              << std::setw(2) << (tm_time.tm_mon + 1) << '-' << std::setw(2)
              << tm_time.tm_mday << 'T' << std::setw(2) << tm_time.tm_hour << ':'
              << std::setw(2) << tm_time.tm_min << ':' << std::setw(2)
-             << tm_time.tm_sec << "Z";
+             << tm_time.tm_sec << "Z" << "\"";
       break;
     }
     case Logger::Format::kISO8601: {
@@ -173,10 +173,10 @@ LogMessage::~LogMessage()
                                       ? TritonJson::SerializeString(heading_)
                                       : heading_;
     log_record << ", \"message\": " << escaped_heading << ", \"kubernetes\": { \"namespace_name\": " <<
-      getRapidminerTenant() << ", \"app_id\": " << std::getenv("APP_ID") << "}, \"logversion\": 1.0.0}";
+       "\"" << getRapidminerTenant() << "\"" << ", \"app_id\": " << "\"" << std::getenv("APP_ID") << "\"" << "}, \"logversion\": \"1.0.0\"}";
   }
   log_record << ", \"message\": " << escaped_message << ", \"kubernetes\": { \"namespace_name\": " <<
-      getRapidminerTenant() << ", \"app_id\": " << std::getenv("APP_ID") << "}, \"logversion\": 1.0.0}";
+       "\"" << getRapidminerTenant() << "\"" << ", \"app_id\": " << "\"" << std::getenv("APP_ID") << "\"" << "}, \"logversion\": \"1.0.0\"}";
   gLogger_.Log(log_record.str());
 }
 }}  // namespace triton::common
